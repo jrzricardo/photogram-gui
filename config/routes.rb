173e2ba0a3fbeb549bc596d3e408f657b8d1040d
Ricardo Juarez <146899774+jrzricardo@users.noreply.github.com>
2024-06-18 18:48:43 +0000

    adds users#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
 
+  get("/users", { :controller => "users", :action => "index" })
 end
+
+
+# /users 
+# display all the users
+# a link to get to detals for wach user
+# a form to add a new user
+
+# /users/[USERNAME] should
+# display the username of the user
+# the photos posted by the user
+
+# /photos should have a form to add a new photo
+
+# /photos/[ID] should
+# display the details of a photo
+# displays the comments that have been made on the photo
+# have a form to add a comment to the photo
