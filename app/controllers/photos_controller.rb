10cbc8aaca5bd816903ea3f59593ec1a0b133b1d
Ricardo Juarez <146899774+jrzricardo@users.noreply.github.com>
2024-06-20 17:31:16 +0000

    building photo#show

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -7,6 +7,8 @@ class PhotosController < ApplicationController
   end
 
   def show
+    photo_id = params.fetch("path_id")
+    @matching_photo = Photo.where({ :id => photo_id})
     
     render({ :template => "photo_templates/show"})
   end
